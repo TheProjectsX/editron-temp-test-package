cef653ad2db2ce20c6b26df958bc670be7fcd9bd
Modasser Hasan <manvstech2.0@gmail.com>
2025-06-01 00:49:28 +0600

    Project Initialized

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-200 py-5">
+            <div className="bg-white rounded-2xl max-w-2xl w-full p-8"></div>
+        </div>
+    );
+};
+
+export default App;
